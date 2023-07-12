78b535c71d96af0a4f959cc95cfd55d427b1c207
PoopFace <danilsteam-88@hotmail.com>
2023-07-13 00:52:17 +0300

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import react, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState("");
+  const [invalid, setInvalid] = useState(false);
+  const [subbed, setSubbed] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className="main-container">
+      {!subbed ? (
+        <div className="default-stuff-container">
+          <div className="info-container">
+            <h1>Stay updated!</h1>
+            <p>Join 60,000+ product managers receiving monthly updates on:</p>
+            <ul>
+              <li>Product discovery and building what matters</li>
+              <li>Measuring to ensure updates are a success</li>
+              <li>And much more!</li>
+            </ul>
+            <form>
+              <div className="input-labels">
+                <h2>Email address</h2>
+                <h2
+                  style={
+                    invalid
+                      ? {
+                          color: "var(--tomato)",
+                        }
+                      : { display: "none" }
+                  }
+                >
+                  Valid email required
+                </h2>
+              </div>
+              <input
+                type="email"
+                placeholder="email@company.com"
+                style={
+                  invalid
+                    ? {
+                        border: "1px solid var(--tomato)",
+                        color: "var(--tomato)",
+                        backgroundColor: "var(--tomato-tp)",
+                      }
+                    : {}
+                }
+                value={input}
+                onChange={async (e) => {
+                  await setInput(e.target.value);
+                  await setInvalid(false);
+                }}
+              />
+              <button
+                onClick={async (e) => {
+                  e.preventDefault();
+                  if (!input) {
+                    await setInvalid(true);
+                  } else if (
+                    !input.match(
+                      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+                    )
+                  ) {
+                    await setInvalid(true);
+                  } else {
+                    await setSubbed(true);
+                  }
+                }}
+              >
+                Subscribe to monthly newsletter
+              </button>
+            </form>
+          </div>
+          <div className="img-container">
+
+          </div>
+          {/* <img src="/assets/images/illustration-sign-up-desktop.svg" /> */}
+        </div>
+      ) : (
+        <div className="subbed-stuff-container">
+          <div className="info-container">
+            <h1>Thanks for subscribing!</h1>
+            <p>
+              A confirmation email has been sent to <span>{input}</span>. Please
+              open it and click the button inside to confirm your subscription.
+            </p>
+            <button onClick={async () => {await setInput("")
+              await setInvalid(false)
+              await setSubbed(false)
+            }}>Dismiss message</button>
+          </div>
+        </div>
+      )}
+    </main>
+  );
 }
 
-export default App
+export default App;
